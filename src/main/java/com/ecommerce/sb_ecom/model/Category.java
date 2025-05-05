4caa340491bd97645067911284fa422f21a7dea9
Mohit Chhabria <mohitc183@gmail.com>
2025-05-05 14:48:08 +0530

    Added Lombok annotations to Category entity to reduce boilerplate code

--- src/main/java/com/ecommerce/sb_ecom/model/Category.java
+++ src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -1,38 +1,46 @@
 package com.ecommerce.sb_ecom.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "Categories")
+@Entity(name = "Categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
-
-    public Category() {
-
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String categoryName;
 
-    public Category(Long id, String categoryName) {
-        this.id = id;
-        this.categoryName = categoryName;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
+
+
+
+//    public String getCategoryName() {
+//        return categoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName) {
+//        this.categoryName = categoryName;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+//    public Category() {
+//
+//    }
+
+//    public Category(Long id, String categoryName) {
+//        this.id = id;
+//        this.categoryName = categoryName;
+//    }
