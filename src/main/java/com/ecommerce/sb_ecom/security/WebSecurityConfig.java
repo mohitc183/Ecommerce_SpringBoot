0eaf49b0d3cd7620f2ec2c90df1dc4849f8fd088
Mohit Chhabria <mohitc183@gmail.com>
2025-05-28 21:09:48 +0530

    Implemented Cookie based Authentication in JWT

--- src/main/java/com/ecommerce/sb_ecom/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/sb_ecom/security/WebSecurityConfig.java
@@ -1,9 +1,15 @@
 package com.ecommerce.sb_ecom.security;
 
+import com.ecommerce.sb_ecom.model.AppRole;
+import com.ecommerce.sb_ecom.model.Role;
+import com.ecommerce.sb_ecom.model.User;
+import com.ecommerce.sb_ecom.repository.RoleRepository;
+import com.ecommerce.sb_ecom.repository.UserRepository;
 import com.ecommerce.sb_ecom.security.jwt.AuthEntryPointJwt;
 import com.ecommerce.sb_ecom.security.jwt.AuthTokenFilter;
 import com.ecommerce.sb_ecom.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,6 +27,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -53,6 +61,7 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
@@ -70,16 +79,18 @@ public class WebSecurityConfig {
             authorizeHttpRequests(auth ->
                     auth.requestMatchers("/api/auth/**").permitAll()
                     .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/api/public/**").permitAll()
-                        .requestMatchers("/api/admin/**").permitAll()
+//                        .requestMatchers("/api/public/**").permitAll()
+//                        .requestMatchers("/api/admin/**").permitAll()
                         .requestMatchers("/api/test/**").permitAll()
                         .requestMatchers("/images/**").permitAll()
                     .anyRequest().authenticated());
 
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authenticationJWTTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
+        http.headers( headers -> headers.frameOptions(
+                frameOptions -> frameOptions.sameOrigin()));
 
         return http.build();
     }
@@ -93,5 +104,68 @@ public class WebSecurityConfig {
                 "/configuration/security",
                 "/swagger-ui.html",
                 "/webjars/**"));
+    }
+
+    @Bean
+    public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+
+        return args -> {
+
+            // Retrieve or create roles
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseGet(() -> {
+                        Role newUserRole = new Role(AppRole.ROLE_USER);
+                        return roleRepository.save(newUserRole);
+                    });
+
+            Role sellerRole = roleRepository.findByRoleName(AppRole.ROLE_SELLER)
+                    .orElseGet(() -> {
+                        Role newSellerRole = new Role(AppRole.ROLE_SELLER);
+                        return roleRepository.save(newSellerRole);
+                    });
+
+            Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                    .orElseGet(() -> {
+                        Role newAdminRole = new Role(AppRole.ROLE_ADMIN);
+                        return roleRepository.save(newAdminRole);
+                    });
+
+            Set<Role> userRoles = Set.of(userRole);
+            Set<Role> sellerRoles = Set.of(sellerRole);
+            Set<Role> adminRoles = Set.of(userRole, sellerRole, adminRole);
+
+
+            // Create users if not already present
+            if (!userRepository.existsByUserName("user1")) {
+                User user1 = new User("user1", "user1@example.com", passwordEncoder.encode("password1"));
+                userRepository.save(user1);
+            }
+
+            if (!userRepository.existsByUserName("seller1")) {
+                User seller1 = new User("seller1", "seller1@example.com", passwordEncoder.encode("password2"));
+                userRepository.save(seller1);
+            }
+
+            if (!userRepository.existsByUserName("admin")) {
+                User admin = new User("admin", "admin@example.com", passwordEncoder.encode("adminPass"));
+                userRepository.save(admin);
+            }
+
+            // Update roles for existing users
+            userRepository.findByUserName("user1").ifPresent(user -> {
+                user.setRoles(userRoles);
+                userRepository.save(user);
+            });
+
+            userRepository.findByUserName("seller1").ifPresent(seller -> {
+                seller.setRoles(sellerRoles);
+                userRepository.save(seller);
+            });
+
+            userRepository.findByUserName("admin").ifPresent(admin -> {
+                admin.setRoles(adminRoles);
+                userRepository.save(admin);
+            });
+        };
     }
 }
--- src/main/java/com/ecommerce/sb_ecom/security/request/LoginRequest.java
+++ src/main/java/com/ecommerce/sb_ecom/security/request/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.ecommerce.sb_ecom.security.request;
 
 public class LoginRequest {
+
     private String username;
 
     private String password;
