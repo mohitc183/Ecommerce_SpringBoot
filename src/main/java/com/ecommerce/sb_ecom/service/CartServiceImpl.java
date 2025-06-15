55f17762fbb1de5b6ee7467e4c60dcd0d663e3fb
Mohit Chhabria <mohitc183@gmail.com>
2025-06-15 16:48:57 +0530

    Adding product to the cart & implementing AuthUtils class

--- src/main/java/com/ecommerce/sb_ecom/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/sb_ecom/service/CartServiceImpl.java
@@ -1,20 +1,34 @@
 package com.ecommerce.sb_ecom.service;
 
+import com.ecommerce.sb_ecom.exceptions.APIException;
 import com.ecommerce.sb_ecom.exceptions.ResourceNotFoundException;
 import com.ecommerce.sb_ecom.model.Cart;
+import com.ecommerce.sb_ecom.model.CartItem;
 import com.ecommerce.sb_ecom.model.Product;
 import com.ecommerce.sb_ecom.payload.CartDTO;
+import com.ecommerce.sb_ecom.payload.ProductDTO;
+import com.ecommerce.sb_ecom.repository.CartItemRepository;
 import com.ecommerce.sb_ecom.repository.CartRepository;
 import com.ecommerce.sb_ecom.repository.ProductRepository;
-import com.ecommerce.sb_ecom.security.response.MessageResponse;
+import com.ecommerce.sb_ecom.util.AuthUtil;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class CartServiceImpl implements CartService {
 
     @Autowired
     CartRepository cartRepository;
+
+    @Autowired
+    CartItemRepository cartItemRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
 
     @Autowired
     AuthUtil authUtil;
@@ -28,14 +42,66 @@ public class CartServiceImpl implements CartService {
         //Find existing cart or create one
         Cart cart = createCart();
 
+
         //Retrieve Product Details
         Product product = productRepository.findById(productId)
-                .orElseThrow( () -> new ResourceNotFoundException("Product","productId", productId));
+                .orElseThrow( () -> new ResourceNotFoundException("Product","productId", String.valueOf(productId)));
+
+
         //Perform Validations
+        CartItem cartItem = cartItemRepository.findCartItemByIdAndCaertId(
+                cart.getCartId(),
+                productId
+        );
+
+        if(cartItem != null){
+            throw new APIException("Product " + product.getProductName() + " already exists in the cart!!");
+        }
+
+        if(product.getQuantity() == 0){
+            throw new APIException(product.getProductName() + " is not available!!");
+        }
+
+        if(product.getQuantity() < quantity){
+            throw new APIException("Please make an order for " + product.getProductName() + " less than or equal to " +
+                    "quantity " + product.getQuantity() + "." );
+        }
+
+
         //Create cart item
+        CartItem newCartItem = new CartItem();
+        newCartItem.setProduct(product);
+        newCartItem.setCart(cart);
+        newCartItem.setQuantity(quantity);
+        newCartItem.setDiscount(product.getDiscount());
+        newCartItem.setProductPrice(product.getSpecialPrice());
+
+
         //Save cart item
+        cartItemRepository.save(newCartItem);
+
+        product.setQuantity(product.getQuantity());
+        cart.setTotalPrice(cart.getTotalPrice() + (product.getSpecialPrice() * quantity));
+
+
+        //Save cart
+        cartRepository.save(cart);
+
+
         //Return updated cart information
-        return null;
+        CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+
+        List<CartItem> cartItems = cart.getCartItems();
+
+        Stream<ProductDTO> productDTOStream = cartItems.stream().map(item -> {
+            ProductDTO map = modelMapper.map(cartItem.getProduct(), ProductDTO.class);
+            map.setQuantity(item.getQuantity());
+            return map;
+        });
+
+        cartDTO.setProducts(productDTOStream.toList());
+
+        return cartDTO;
     }
 
     private Cart createCart(){
